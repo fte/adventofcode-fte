3d50de457d47e62d8c7e9e5449234008e8189db8
fte <github@575.be>
2019-12-06 12:48:30 +0100

    refactor

--- 2019/5b.php
+++ 2019/5b.php
@@ -15,7 +15,10 @@ while (99 != $aIntCodes[$position]) {
     // echo "opcode=$opcode\n";
     // print_r($aModes);
     if(in_array($opcode, array(1,2,5,6,7,8))) {
-       
+        $p1= $aIntCodes[$position+1];
+        $p2= $aIntCodes[$position+2];
+        $v1= ($aModes[0] == 1) ? $p1 : $aIntCodes[$p1];   
+        $v2= ($aModes[1] == 1) ? $p2 : $aIntCodes[$p2];
         // echo $p1."(".(int)$aModes[0]."):$v1 [+*] ".$p2."(".(int)$aModes[1]."):$v2\n";
     } elseif(in_array($opcode, array(4))) {
         $p1= $aIntCodes[$position+1];
@@ -26,10 +29,6 @@ while (99 != $aIntCodes[$position]) {
     $new_position= $position;
     switch ($opcode) {
         case '1':
-            $p1= $aIntCodes[$position+1];
-            $p2= $aIntCodes[$position+2];
-            $v1= ($aModes[0] == 1) ? $p1 : $aIntCodes[$p1];   
-            $v2= ($aModes[1] == 1) ? $p2 : $aIntCodes[$p2];
             $position_cible= $aIntCodes[$position+3];
 
             $res=  $v1 + $v2;
@@ -37,10 +36,6 @@ while (99 != $aIntCodes[$position]) {
             $pointeur=4;
             break;
         case '2':
-            $p1= $aIntCodes[$position+1];
-            $p2= $aIntCodes[$position+2];
-            $v1= ($aModes[0] == 1) ? $p1 : $aIntCodes[$p1];   
-            $v2= ($aModes[1] == 1) ? $p2 : $aIntCodes[$p2];
             $position_cible= $aIntCodes[$position+3];
 
             $res=  $v1 * $v2;
@@ -59,10 +54,6 @@ while (99 != $aIntCodes[$position]) {
             $pointeur=2;
             break;
         case '5':
-            $p1= $aIntCodes[$position+1];
-            $p2= $aIntCodes[$position+2];
-            $v1= ($aModes[0] == 1) ? $p1 : $aIntCodes[$p1];   
-            $v2= ($aModes[1] == 1) ? $p2 : $aIntCodes[$p2];
             if ($v1 != 0) {
                 $new_position= $v2;
             } else {
@@ -70,10 +61,6 @@ while (99 != $aIntCodes[$position]) {
             }
             break;
         case '6':
-            $p1= $aIntCodes[$position+1];
-            $p2= $aIntCodes[$position+2];
-            $v1= ($aModes[0] == 1) ? $p1 : $aIntCodes[$p1];   
-            $v2= ($aModes[1] == 1) ? $p2 : $aIntCodes[$p2];
             if ($v1 == 0) {
                 $new_position= $v2;
             } else {
@@ -81,10 +68,6 @@ while (99 != $aIntCodes[$position]) {
             }
             break;
         case '7':
-            $p1= $aIntCodes[$position+1];
-            $p2= $aIntCodes[$position+2];
-            $v1= ($aModes[0] == 1) ? $p1 : $aIntCodes[$p1];   
-            $v2= ($aModes[1] == 1) ? $p2 : $aIntCodes[$p2];
             $position_cible= $aIntCodes[$position+3];
 
             if ($v1 < $v2) {
@@ -95,10 +78,6 @@ while (99 != $aIntCodes[$position]) {
             $pointeur=4;
             break;
         case '8':
-            $p1= $aIntCodes[$position+1];
-            $p2= $aIntCodes[$position+2];
-            $v1= ($aModes[0] == 1) ? $p1 : $aIntCodes[$p1];   
-            $v2= ($aModes[1] == 1) ? $p2 : $aIntCodes[$p2];
             $position_cible= $aIntCodes[$position+3];
 
             if ($v1 == $v2) {
