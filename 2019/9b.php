195b6d451d0852331f050d3e1e16345d48e0faa2
fte <github@575.be>
2019-12-17 16:37:08 +0100

    boost

---
+++ 2019/9b.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php -q
+<?php
+/**
+ * Intcode
+ */
+require dirname(__FILE__).'/IntCode.php';
+
+array_shift($argv);
+$aIntCodes= preg_split('#,#', $argv[0]);
+
+$intcode= new IntCode($aIntCodes, array(2));
+// $intcode= new IntCode($aIntCodes);
+$out= array();
+
+$i=0;
+do {
+    $i+=1;
+    // echo "i=$i\n";
+    $output= $intcode->compute();
+    echo " output=$output\n";
+    $out[] = $output;
+
+    // if($intcode->position < 300) die();
+
+} while ($output !== null);
+
+echo implode('', $out);
