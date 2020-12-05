6b6e625be815faa29773c911be4360de39499b29
fte <github@575.be>
2020-12-05 12:58:10 +0100

    aoc2020 day 5  part 1

--- 2020/5a.jl
+++ 2020/5a.jl
@@ -1,10 +1,14 @@
 #!/usr/bin/env julia
 
 codes = readlines(ARGS[1])
-println(codes)
-code= "FFFBBBFRRR"
+# code= "FFFBBBFRRR"
 trads = Dict('F' => '0', 'B' => '1', 'R' => '1', 'L' => '0')
-bin= join([trads[c] for c in code])
-println(bin,' ',typeof(bin))
-# println(Int(string("0b",bin)))
-println(parse(Int, bin; base=2))
+max = 0
+for code in codes
+    bin = join([trads[c] for c in code])
+    dec = parse(Int, bin; base=2)
+    if (dec > max)
+        global max = dec
+    end
+end
+println(max)
