eaf99e3a2ecdf0ab378fa22f4b2580c32b6504bf
fte <github@575.be>
2020-12-07 10:10:01 +0100

    aoc2020 day part 2

---
+++ 2020/6b.jl
@@ -0,0 +1,21 @@
+#!/usr/bin/env julia
+
+reponses = split(chomp(read(ARGS[1], String)), "\n\n")
+# println(reponses)
+
+nbYesByChar = Dict{Char,Int}()
+global total = 0 
+a2z= "abcdefghijklmnopqrstuvwxyz"
+for groupe in reponses
+    aGrp = split(groupe, "\n")
+    aGrp =  Array{Char,1}[collect(g) for g in aGrp]
+    nbPersonnes = length(aGrp)
+    for c in a2z
+        nbLettre = count(i -> (i == c), groupe)	
+        # println("grp=",groupe," lettre=",c," nbLettre=",nbLettre)
+		if nbLettre == nbPersonnes
+			global total += 1
+		end
+    end
+end
+println(total)
