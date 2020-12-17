71c7bea8280927706a765d4bfafa975a5f781b92
fte <github@575.be>
2020-12-17 17:25:26 +0100

    aoc2020 day 13 part 1

---
+++ 2020/13a.jl
@@ -0,0 +1,15 @@
+#!/usr/bin/env julia
+
+l = readlines(ARGS[1])
+depart = parse(Int, l[1])
+bus_brut = split(l[2], ',')
+bus_id = findall(x -> x != "x", bus_brut)
+bus = parse.(Int, bus_brut[bus_id])
+
+# println("depart=",depart)
+# println("map=", map(x -> ceil(depart / x) * x - depart, bus))
+# println("cld=", cld.(depart,bus) )
+# println("cld=", cld.(depart,bus) .* bus .- depart )
+# println("fm=", findmin(cld.(depart,bus) .* bus .- depart ))
+attente,bus_id= findmin(cld.(depart,bus) .* bus .- depart )
+println(attente * bus[bus_id])
