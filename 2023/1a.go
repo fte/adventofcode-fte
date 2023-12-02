080ac858893be9964237d94b2f46cdd1d558a318
fte <github@575.be>
2023-12-02 14:12:57 +0100

    1er jour gogogo

---
+++ 2023/1a.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+	"strconv"
+)
+
+func main() {
+	// file, err := os.Open("input1a.test.txt")
+	file, err := os.Open("input1a.txt")
+	if err != nil {
+		fmt.Println("Erreur lors de l'ouverture du fichier :", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var somme int
+	for scanner.Scan() {
+		line := scanner.Text()
+		// fmt.Println(line)
+		var premierChiffre, dernierChiffre rune
+		for _, char := range line {
+			if unicode.IsDigit(char) {
+				if(premierChiffre == 0) {
+					premierChiffre = char
+				}
+				dernierChiffre= char //remplacé jusqu'au dernier
+			}
+		}
+
+		// fmt.Println(string(premierChiffre)+string(dernierChiffre))
+		resultat, err := strconv.Atoi(string(premierChiffre)+string(dernierChiffre))
+		if err != nil {
+			fmt.Println("Erreur de conversion en entier :", err)
+			return
+		}
+		somme = somme + resultat
+		// fmt.Println("resultat",resultat)
+		fmt.Println("resultat",somme)
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier :", err)
+	}
+}
